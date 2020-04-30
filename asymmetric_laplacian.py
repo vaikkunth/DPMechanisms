603e1401f4a992e50e64eafe083b795c4428c064
Wanyi Xiao <wanyi@mit.edu>
2020-04-30 16:36:20 -0400

    example for testing added for asymLap and truncatedLap

--- asymmetric_laplacian.py
+++ asymmetric_laplacian.py
@@ -1,12 +1,13 @@
 import scipy.integrate as integrate
 import numpy as np
+from math import inf
 
 '''
-Given privacy parameters epsilon, delta, and scale parameter k, return the function for the probability density function 
+Given privacy parameters epsilon, global_sensitivity, and scale parameter k, return the function for the probability density function 
 for asymmetric laplace distribution.
 '''
-def asymmetric_laplace(epsilon, delta, k):
-    lam = epsilon / delta
+def asymmetric_laplace(epsilon, global_sensitivity, k):
+    lam = epsilon / global_sensitivity
     def f(x):
         if x < 0:
             return lam / (k + 1 / k) * np.exp(lam * x / k)
@@ -16,14 +17,26 @@ def asymmetric_laplace(epsilon, delta, k):
 
 
 '''
-Given epsilon, delta, and k, return the L1 cost for the asymmetric laplacian mechanism.
+Given epsilon, global_sensitivity, and k, return the L1 cost for the asymmetric laplacian mechanism.
 '''
-def truncated_laplace_L1_eval(epsilon, delta, k):
-    return integrate.quad(lambda x: abs(x) * asymmetric_laplace(epsilon, delta, k)(x))[0]
+def asymmetric_laplace_L1_eval(epsilon, global_sensitivity, k):
+    return integrate.quad(lambda x: abs(x) * asymmetric_laplace(epsilon, global_sensitivity, k)(x), -inf, inf)[0]
 
 
 '''
-Given epsilon, delta, and k, return the L2 cost for the asymmetric laplacian mechanism.
+Given epsilon, global_sensitivity, and k, return the L2 cost for the asymmetric laplacian mechanism.
 '''
-def truncated_laplace_L2_eval(epsilon, delta, k):
-    return integrate.quad(lambda x: x**2 * asymmetric_laplace(epsilon, delta,k)(x))[0]
+def asymmetric_laplace_L2_eval(epsilon, global_sensitivity, k):
+    return integrate.quad(lambda x: x**2 * asymmetric_laplace(epsilon, global_sensitivity,k)(x), -inf, inf)[0]
+
+
+# Example for getting resutls for asymmetric laplacian
+def asymmetric_laplacian_example():
+	epsilons = list(np.arange(1e-4, 1e-3, 1e-4)) # change here for different epsilon values
+	global_sensitivity = 1 # changee here for different global sensitivity values
+	ks = list(np.arange(2, 3, 1)) # change here for different scale parameter values
+	print(['epsilon', 'global sensitivity', 'scale parameter(k)', 'L_1 cost', 'L_2 cost'])
+	for epsilon in epsilons:
+		for k in ks: 	
+			print([epsilon, global_sensitivity, k, asymmetric_laplace_L1_eval(epsilon, global_sensitivity, k), asymmetric_laplace_L2_eval(epsilon, global_sensitivity, k)])
+
--- truncated_laplacian.py
+++ truncated_laplacian.py
@@ -6,37 +6,48 @@ This is a helper function for generating the probability density function for tr
 This function processes A and B to be the desired value. If the input A, B value is not appropriate for the desired 
 epsilon, we default to the values for symmetric A, B values. 
 '''
-def process(epsilon, delta, A, B):
+def process(epsilon, global_sensitivity, A, B):
     lam = 1 / epsilon
-    A_ = lam * np.log(2 + (1 - delta) / delta * np.exp(-B / lam) - 1 / delta * np.exp(-(B - 1) / lam))
+    A_ = lam * np.log(2 + (1 - global_sensitivity) / global_sensitivity * np.exp(-B / lam) - 1 / global_sensitivity * np.exp(-(B - 1) / lam))
     if abs(B) < abs(A_):
         return A_, B
-    B_ = -lam * np.log(2 + (1 - delta)/delta * np.exp(A/lam) - 1/delta * np.exp((B+1)/lam))
+    B_ = -lam * np.log(2 + (1 - global_sensitivity)/global_sensitivity * np.exp(A/lam) - 1/global_sensitivity * np.exp((B+1)/lam))
     if abs(A) < abs(B_):
         return A, B_
-    A_ = delta / epsilon * np.log(1 + (np.exp(epsilon) - 1)/(2 * delta))
+    A_ = global_sensitivity / epsilon * np.log(1 + (np.exp(epsilon) - 1)/(2 * global_sensitivity))
     B_ = - A_
     return A_, B_
 
 '''
-Given privacy parameters epsilon, delta, and scale parameters A, and B, return a function that is the probability 
+Given privacy parameters epsilon, global_sensitivity, and scale parameters A, and B, return a function that is the probability 
 density function for the truncated laplace distribution.
 '''
-def truncated_laplace(epsilon, delta, A, B):
+def truncated_laplace(epsilon, global_sensitivity, A, B):
     lam = 1 / epsilon
-    a, b = process(epsilon, delta, A, B)
-    M = 1 / (lam * (2 - np.exp(a / lam) - np.exp(-b / lam)))
+    M = 1 / (lam * (2 - np.exp(A / lam) - np.exp(-B / lam)))
     return lambda x: M * np.exp(-abs(x) / lam)
 
 '''
-Given epsilon, delta, A, B, return the L1 cost for the truncated laplacian mechanism.
+Given epsilon, global_sensitivity, A, B, return the L1 cost for the truncated laplacian mechanism.
 '''
-def truncated_laplace_L1_eval(epsilon, delta, A, B):
-    return integrate.quad(lambda x: abs(x) * truncated_laplace(epsilon, delta, A, B)(x), A, B)[0]
+def truncated_laplace_L1_eval(epsilon, global_sensitivity, A, B):
+    return integrate.quad(lambda x: abs(x) * truncated_laplace(epsilon, global_sensitivity, A, B)(x), A, B)[0]
 
 '''
-Given epsilon, delta, A, B, return the L2 cost for the truncated laplacian mechanism.
+Given epsilon, global_sensitivity, A, B, return the L2 cost for the truncated laplacian mechanism.
 '''
-def truncated_laplace_L2_eval(epsilon, delta, A, B):
-    return integrate.quad(lambda x: x**2 * truncated_laplace(epsilon, delta, A, B)(x), A, B)[0]
+def truncated_laplace_L2_eval(epsilon, global_sensitivity, A, B):
+    return integrate.quad(lambda x: x**2 * truncated_laplace(epsilon, global_sensitivity, A, B)(x), A, B)[0]
+
+# Example for getting resutls for truncated laplacian
+def truncated_laplacian_example(): 
+    epsilons = list(np.arange(1e-4, 1e-3, 1e-4)) # change here for different epsilon values
+    global_sensitivity = 1 # change here for different global sensitivity
+    A = list(np.arange(-10, 10, 2)) # change here for different left truncation values
+    print(['epsilon', 'global sensitivity', 'left truncation', 'right truncation' 'L_1 cost', 'L_2 cost'])
+    for i in range(min(len(epsilons), len(A))):
+        a, b = process(epsilons[i], global_sensitivity, A[i], -A[i]) # It is necessary to call process function to get the appropriate values
+        print([epsilons[i], global_sensitivity, a, b, truncated_laplace_L1_eval(epsilons[i], global_sensitivity, a, b), truncated_laplace_L2_eval(epsilons[i], global_sensitivity, a, b)])
+
+truncated_laplacian_example()
 
